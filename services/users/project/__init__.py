f0373fd86e8652472cee8b65651adea43e3ac2f1
Joseph Diku <joseph.diku@gmail.com>
2019-08-08 15:17:00 +0300

    Register project configuration for Development environment into the main app

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -4,6 +4,9 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+# set config
+app.config.from_object('project.config.DevelopmentConfig')
+
 
 class UserAuthentication(Resource):
     def get(self):
