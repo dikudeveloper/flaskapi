c8343856368c5e1526a5b862f720364f7f593530
Joseph Diku <joseph.diku@gmail.com>
2019-08-08 17:00:11 +0300

    Add SQLAlchemy to main flask application

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,6 +10,23 @@ api = Api(app)
 # set config
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
+
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+
+# Model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
 class UserAuthentication(Resource):
